1f950674b224d64a08371affaf0e0567df90ccc2
MIchowdhury <masum.iqbal.chowdhury@gmail.com>
2025-02-21 23:09:42 +0600

    modified

--- script.js
+++ script.js
@@ -25,5 +25,77 @@ addTaskBnt.addEventListener("click", toggleAddTaskForm);
 blackBackdrop.addEventListener("click", toggleAddTaskForm);
 
 
+// lets add categories and tasks with js
+
+let categories = [
+  {
+    title: "Personal",
+    img: "user.jpg",
+  },
+  {
+    title: "Work",
+    img: "briefcase.jpg",
+  },
+  {
+    title: "Shopping",
+    img: "shopping-bag.jpg",
+  },
+  {
+    title: "Health",
+    img: "health.jpg",
+  },
+  {
+    title: "Fitness",
+    img: "fitness.jpg",
+  },
+  {
+    title: "Education",
+    img: "education.jpg",
+  },
+  {
+    title: "Finance",
+    img: "finance.jpg",
+  },
+  
+];
+ 
+let tasks = [
+  {
+    id: 1,
+    task: "Go to market",
+    category: "Shopping",
+    completed: false,
+  },
+  {
+    id: 2,
+    task: "Read chapter of a book",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 3,
+    task: "Prepare presentation for meeting",
+    category: "Work",
+    completed: false,
+  },
+  {
+    id: 4,
+    task: "Complete coding challenge",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 5,
+    task: "Do a 20-",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 6,
+    task: "Read chapter of a book",
+    category: "Personal",
+    completed: false,
+  },
+];
 
 
